b8ee02832f51b4e548808f2f03ca32018ecbcf68
lumixus <44450220+lumixus@users.noreply.github.com>
2023-09-17 15:59:09 +0300

    Init

---
+++ websocket.js
@@ -0,0 +1,25 @@
+import express from "express"
+import {createServer} from "node:http"
+import {Server} from "socket.io"
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('newConnection', {msg: `${socket.id} connected`});
+    io.emit('get-client-count', {clientCount: io.engine.clientsCount})
+    socket.on('chat message', (msg) =>  {
+        io.emit('new message', {msg: msg, sender: socket.id});
+    })
+    console.log(`a user connected to ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('userDisconnected', {msg: `${socket.id} disconnected`, clientCount: io.engine.clientsCount});
+    })
+})
+
+
+server.listen(8080, () => {
+    console.log("Websocket server started at localhost:8080");
+})
